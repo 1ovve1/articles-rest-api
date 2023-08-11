75cf85a7b15bf64938fa8ae7f896b355a7ec67d2
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2023-08-12 01:30:36 +0300

    Add Auth In Routes

--- routes/version/v1.php
+++ routes/version/v1.php
@@ -1,10 +1,20 @@
 <?php
 
 use App\Http\Controllers\Api\V1\ArticleController;
+use App\Http\Controllers\Api\V1\UserAuthController;
 use App\Http\Controllers\Api\V1\AuthorController;
 use App\Http\Controllers\Api\V1\RubricController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('authors', AuthorController::class);
-Route::apiResource('rubrics', RubricController::class);
-Route::apiResource('articles', ArticleController::class);
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('/',  [UserAuthController::class, 'index'])
+        ->middleware('auth:sanctum');
+    Route::post('/login', [UserAuthController::class, 'login']);
+    Route::post('/register', [UserAuthController::class, 'register']);
+});
+
+Route::apiResources([
+    'authors' => AuthorController::class,
+    'rubrics' => RubricController::class,
+    'articles' => ArticleController::class
+]);
